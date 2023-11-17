d94d2b3eb89373fbdf63c35f375d8b369ada422b
Netradeepak Chinchwadkar <netradeepakc_rocky@localhost.localdomain>
2023-11-17 19:40:32 +0530

    Log Stash

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,14 +1,10 @@
 package main;
 
 import java.util.Scanner;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public class Main {
-    private static final Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args){
         Scanner reader = new Scanner(System.in);
-        logger.info("A fundamental calculator for exponential, nCr and Greatest Common Divisor");
         System.out.println("Welcome to the calculator app");
         int choice = 0;
         while(choice != 99){
@@ -23,7 +19,6 @@ public class Main {
                 System.out.println("Please enter base and exponent");
                 int base = reader.nextInt();
                 int exp = reader.nextInt();
-                logger.info("Now executing power function");
                 int answer = power(base, exp);
                 System.out.println(base + " raise to the power " + exp + " is: ");
                 System.out.println(answer);
@@ -34,7 +29,6 @@ public class Main {
                 int n = reader.nextInt();
                 int r = reader.nextInt();
                 if(n >= r){
-                    logger.info("Now calculating nCr");
                     int answer = nCr(n, r);
                     System.out.println("nCr for n = " + n + " and r = " + r + " is:");
                     System.out.println(answer);
@@ -48,12 +42,11 @@ public class Main {
                 System.out.println("Please enter the two number");
                 int a = reader.nextInt();
                 int b = reader.nextInt();
-                logger.info("Now calculating gcd");
                 System.out.println("GCD of " + a + " and " + b);
                 System.out.println(gcd(a, b));
             }
-            else if(choice == 4){
-
+            else{
+                System.out.println("Invalid Choice.");
             }
         }
     }
